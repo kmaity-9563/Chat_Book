5e04bce81a29affeef098884655614857e3721d0
koushik maity <koushikmaity9563@gmail.com>
2023-12-26 01:14:30 +0530

    header created

--- Frontend/src/components/miscellaneous/SearchDrawer.jsx
+++ Frontend/src/components/miscellaneous/SearchDrawer.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import { useState } from "react";
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Box from '@mui/system/Box';
+import { Typography } from "@mui/material";
+import PositionedMenu from './DashBoard'
 
- function SearchDrawer(){
-    return(
-        <div>
-            <h1>Search Drawer</h1>
-        </div>
-    )
+const SearchDrawer = () => {
+  const [search, setSearch] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [chatLoading, setChatLoading] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{display : "flex", justifyContent : "space-between",  alignItems:"center" , background: "#F3F3F3",width : "100%",}}>
+    <div style={{
+      display : "flex",
+      alignItems: "center",
+      width : "200px",
+      color : "black" , 
+      paddingTop: 5,   
+      paddingLeft: 5,
+       borderWidth: "5px", bgcolor: 'primary.main',
+      '&:hover': { bgcolor: 'primary.dark', }, 
+      
+    }}>
+      <Tooltip title="Search" placement="bottom">
+        <Button>
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+      </Tooltip>
+      <Tooltip title="Search" placement="bottom">
+        <Button onClick={handleClickOpen}>
+          Search User
+        </Button>
+      </Tooltip>
+    </div>
+    <Typography variant="h5" fontFamily="sans-serif" paddingRight={"100px"}>
+      Chat Book
+    </Typography>
+    <div >
+      <PositionedMenu />
+      </div>
+    </div>
+  );
 }
 
 export default SearchDrawer;
